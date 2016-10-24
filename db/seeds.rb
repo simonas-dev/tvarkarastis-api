d3585584971d1e38e1afafa0e5879f63ebee76ab
Simonas <simonas.sank@gmail.com>
2016-10-24 19:41:36 +0300

    Seed performance

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,35 @@
 
 json = ActiveSupport::JSON.decode(File.read('db/seeds/schools.json'))
 
+school_list = []
+
 json.each do |a|
-  school = School.new()
-  if School.find_by(code: a['kodas'])
-		school = School.find_by(code: a['kodas'])
-  end
-  
   puts a['kodas'] 
 
-	school.code = a['kodas']
-	school.name = a['geltoni']['name']
-	school.web = a['geltoni']['web']
+	code = a['kodas']
+	name = a['geltoni']['name']
+	web = a['geltoni']['web']
 
 	if a['geocode'] != {}
-		school.address = a['geocode']['query']
-		school.g_place_id = a['geocode']['place_id']
+		address = a['geocode']['query']
+		g_place_id = a['geocode']['place_id']
 		lat_s = a['geocode']['location']['lat'].to_s
 		lng_s = a['geocode']['location']['lng'].to_s
-		school.g_lat_lng = lat_s + ", " + lng_s
+		g_lat_lng = lat_s + ", " + lng_s
 	else
-		school.address = a['geltoni']['location']['address'] + ", " + a['geltoni']['location']['post_code']
+		address = a['geltoni']['location']['address'] + ", " + a['geltoni']['location']['post_code']
 	end
-	
-	puts school.to_json
 
-  school.save
+	school = {
+		code: code,
+		name: name,
+		web: web,
+		address: address,
+		g_place_id: g_place_id,
+		g_lat_lng: g_lat_lng
+	}
+	
+	school_list.push(school)
 end
+
+School.create(school_list)
